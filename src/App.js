9fb3513c22b88e2a7a7cd83d9b0427da1e13a0d6
barnabas2022 <123511857+barnabas2022@users.noreply.github.com>
2023-04-16 17:39:55 +0100

    remove loading spinner

--- src/App.js
+++ src/App.js
@@ -10,37 +10,33 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const spinner = document.getElementById("spinner");
+  // const [loading, setLoading] = useState(true);
+  // const spinner = document.getElementById("spinner");
 
-  if (loading) {
-    setTimeout(() => {
-      spinner.style.display = "none";
-      setLoading(false);
-    }, 2000);
-  }
+  // if (loading) {
+  //   setTimeout(() => {
+  //     spinner.style.display = "none";
+  //     setLoading(false);
+  //   }, 2000);
+  // }
   return (
     <div className="App">
-      {!loading && (
-        <Router>
-          <Routes>
-            <Route
-              exact
-              path="/start-calculating"
-              element={<CalculatorStart />}
-            />
-            <Route exact path="/" element={<Home />} />
-            <Route
-              exact
-              path="/calculate-cards"
-              element={<CalculatorCards />}
-            />
-            <Route path="/calculate-units" element={<CalculateUnits />} />
-            <Route path="/processing" element={<Processing />} />
-            <Route path="/result" element={<ResultPage />} />
-          </Routes>
-        </Router>
-      )}
+      {/* {!loading && ( */}
+      <Router>
+        <Routes>
+          <Route
+            exact
+            path="/start-calculating"
+            element={<CalculatorStart />}
+          />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/calculate-cards" element={<CalculatorCards />} />
+          <Route path="/calculate-units" element={<CalculateUnits />} />
+          <Route path="/processing" element={<Processing />} />
+          <Route path="/result" element={<ResultPage />} />
+        </Routes>
+      </Router>
+      {/* )} */}
     </div>
   );
 }
