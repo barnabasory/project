2d63158c848f5f423a5e07e3ca76c932a771a480
barnabas2022 <123511857+barnabas2022@users.noreply.github.com>
2023-04-20 16:40:52 +0100

    delete public/_redirects and modifiedd App.js

--- src/App.js
+++ src/App.js
@@ -19,56 +19,50 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    });
+    setIsLoading(false);
   }, []);
   return (
     <>
-      {isLoading ? (
-        <h1>Loadinnnnnnnnnnnnnngggggggggg</h1>
-      ) : (
-        <div className="App">
-          <Router>
-            <Routes>
-              <Route
-                exact
-                path="/start-calculating"
-                element={
-                  isLoading ? <CalculatorStartLoader /> : <CalculatorStart />
-                }
-              />
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route
+              exact
+              path="/start-calculating"
+              element={
+                isLoading ? <CalculatorStartLoader /> : <CalculatorStart />
+              }
+            />
 
-              <Route
-                exact
-                path="/"
-                element={isLoading ? <HomeLoader /> : <Home />}
-              />
+            <Route
+              exact
+              path="/"
+              element={isLoading ? <HomeLoader /> : <Home />}
+            />
 
-              <Route
-                exact
-                path="/calculate-cards"
-                element={
-                  isLoading ? <CalculatorCardsLoader /> : <CalculatorCards />
-                }
-              />
-              <Route
-                exact
-                path="/calculate-units"
-                element={
-                  isLoading ? <CalculateUnitsLoader /> : <CalculateUnits />
-                }
-              />
-              <Route exact path="/processing" element={<Processing />} />
-              <Route
-                exact
-                path="/result"
-                element={isLoading ? <ResultLoader /> : <ResultPage />}
-              />
-            </Routes>
-          </Router>
-        </div>
-      )}
+            <Route
+              exact
+              path="/calculate-cards"
+              element={
+                isLoading ? <CalculatorCardsLoader /> : <CalculatorCards />
+              }
+            />
+            <Route
+              exact
+              path="/calculate-units"
+              element={
+                isLoading ? <CalculateUnitsLoader /> : <CalculateUnits />
+              }
+            />
+            <Route exact path="/processing" element={<Processing />} />
+            <Route
+              exact
+              path="/result"
+              element={isLoading ? <ResultLoader /> : <ResultPage />}
+            />
+          </Routes>
+        </Router>
+      </div>
     </>
   );
 }
